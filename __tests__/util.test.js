8345d377cbfc852fabcdf7c503188e98eefd1469
semnil5202 <semnil5202@gmail.com>
2023-02-09 15:04:57 +0900

    test: 유틸 함수 deepFreeze 테스트 추가

--- __tests__/util.test.js
+++ __tests__/util.test.js
@@ -1,7 +1,7 @@
-const { deepFreeze, generateRandomNumberInRange } = require('../src/utils/common');
+const { deepFreeze, toInt, randomGenerator } = require('../src/utils/common');
 
 describe('유틸 함수 테스트', () => {
-  test('deepFreeze(object): 객체 object를 변경할 수 없게 동결한다.', () => {
+  it('deepFreeze(object): 객체 object를 변경할 수 없게 동결한다.', () => {
     const deepObject = {
       prop1: 'value1',
       prop2: {
@@ -17,17 +17,7 @@ describe('유틸 함수 테스트', () => {
     expect(() => freezeObject()).toThrowError(TypeError);
   });
 
-  describe('generateRandomNumberInRange', () => {
-    test.each([
-      [3, 10],
-      [-1, 10],
-      [0, 1],
-      [-30, -2],
-    ])('getBetween($min, $max): $min 이상 $max 미만인 랜덤 숫자를 반환한다.', (min, max) => {
-      const randomNumber = generateRandomNumberInRange(min, max);
+  it('toInt', () => {});
 
-      expect(randomNumber).toBeGreaterThanOrEqual(min);
-      expect(randomNumber).toBeLessThan(max);
-    });
-  });
+  it('randomGenerator', () => {});
 });
